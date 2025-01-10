e632512d7800b8cb1bf5eaf389844d17b77346ab
callqh <528851597@qq.com>
2025-01-10 16:22:28 +0800

    Revert "Remove directory <path-to-directory>"
    This reverts commit 7d5cb0e71f71997b3cf1e692e67094a12211a5ee.

---
+++ dist/node/cli.js
@@ -0,0 +1,23 @@
+import { cac } from "cac";
+import { dev } from './index.js';
+import { _build } from "./build.js";
+import { serve } from "./server.js";
+const cli = cac('farmpress');
+cli
+    .command('dev', 'start server')
+    .action((dir, options) => {
+    console.log(options);
+    console.log('remove ' + dir);
+    dev();
+});
+cli.command('build', 'build ssg')
+    .action(() => {
+    _build();
+});
+cli.command('preview', 'previw ssg')
+    .action(() => {
+    serve({});
+});
+cli.help();
+cli.parse();
+//# sourceMappingURL=cli.js.map
---
+++ dist/node/dev.js
@@ -0,0 +1,33 @@
+import { start, logger, } from "@farmfe/core";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { cwd } from "process";
+import { farmfePluginMdx } from "../mdx/plugin.js";
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+export const dev = async () => {
+    // 开发环境下当做spa应用去启动
+    // isSSR 始终为false
+    try {
+        await start({
+            compilation: {
+                input: {
+                    index: path.resolve(cwd(), "./index.html")
+                },
+            },
+            server: {
+                port: 6532,
+            },
+            plugins: [
+                '@farmfe/plugin-react',
+                farmfePluginMdx({ isSSR: false })
+            ],
+            publicDir: path.resolve(__dirname, "./public"),
+        });
+    }
+    catch (error) {
+        logger.error(`Failed to start server:\n ${error}`);
+    }
+};
+//# sourceMappingURL=dev.js.map
